f7ae761d9c8f87d1310ed2fac53fc2b925390c56
Maxim (neiromaster) Gavrilenko <parrysh@mail.ru>
2025-01-06 14:39:06 +0500

    feat: Add main entry point to run the application

--- src/radiola/radiola.py
+++ src/radiola/radiola.py
@@ -5,3 +5,12 @@ from textual.widgets import Label
 class RadiolaApp(App):
     def compose(self) -> ComposeResult:
         yield Label("Hello, World!")
+
+
+def run():
+    app = RadiolaApp()
+    app.run()
+
+
+if __name__ == "__main__":
+    run()
